2db348babd1f967c479ef0e6383ef1919a379ff1
Rodney Torres <rtorres1013@github.com>
2017-12-10 16:52:14 -0500

    Startup Matchmaker Homework

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,20 @@
+var product = 10 * 10;
+var quotient = 100/10;
+var difference = 50-17;
+var sum = 50+17;
+var name = 'Rodney' + ' Torres';
+
+console.log(product);
+
+function onra() {
+    var city = 'Saigon';
+    console.log(`Last Tango In ${city}`);
+    
+    
+}
+
+onra();
+onra();
+onra();
+
+console.log(document.getElementById('hello'));
